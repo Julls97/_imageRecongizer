2ebebc9fe254da085fb0779e71d6c815f5966889
Julls97 <yulia.jolie@yandex.ru>
2018-09-19 16:54:40 +0500

    first commit

---
+++ ImageReconizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageReconizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worldvvv!");
+        }
+    }
+}
