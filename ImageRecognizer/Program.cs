7561921290dae07e7a60c3c6b395ba41f9d068f7
kirillova.julia97 <kirillova.julia97@gmail.com>
2018-09-23 22:18:37 +0500

    new class2

---
+++ ImageRecognizer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ImageRecognizer {
+	internal class Program {
+		public static void Main(string[] args) {
+			Bitmap bitmap = new Bitmap("file.bmp");
+			int x = bitmap.Width;
+			int y = bitmap.Height;
+			byte[,] arr = new byte[x,y];
+			for (int i = 0; i < x; i++) {
+				for (int j = 0; j < y; j++) {
+					Color pixel = bitmap.GetPixel(i, j);
+					arr[i, j] = pixel.ToArgb() == Color.White.ToArgb() ? (byte) 0 : (byte) 1;
+					Console.Write(arr[i,j]);
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
